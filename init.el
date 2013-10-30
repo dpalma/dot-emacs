7ae3c9002143c42a9a1099fbad9386a37df297c5
Dan Palma <dan.palma@mathworks.com>
2013-10-30 09:48:54 -0400

    Added multiple-cursors as a submodule

--- init.el
+++ init.el
@@ -5,3 +5,10 @@
 
 (require 'linum)
 (add-hook 'find-file-hook (lambda() (linum-mode 1)))
+
+(add-to-list 'load-path "~/.emacs.d/multiple-cursors/")
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
