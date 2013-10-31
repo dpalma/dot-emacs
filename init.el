ddd696dce162673a5dbc5fe11f2c92b80dd3f33e
Dan Palma <dan.palma@mathworks.com>
2013-10-31 15:08:46 -0400

    Change frame-title-format to show the current buffer's file name

--- init.el
+++ init.el
@@ -12,3 +12,8 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(setq frame-title-format
+      '("" invocation-name ": "(:eval (if (buffer-file-name)
+                                          (abbreviate-file-name (buffer-file-name))
+                                        "%b"))))
