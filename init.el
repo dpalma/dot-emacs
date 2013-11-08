4817e0a68c80c5767f78db9b40d33e3595e09ffd
Dan Palma <dan.palma@mathworks.com>
2013-11-08 09:13:29 -0500

    Added a multiple-cursor keyboard shortcut
    Added a keyboard shortcut that adds a cursor to all selected lines
    Ctrl-Shift S C -- 2X

--- init.el
+++ init.el
@@ -9,6 +9,9 @@
 (add-to-list 'load-path "~/.emacs.d/multiple-cursors/")
 (require 'multiple-cursors)
 
+; Add a cursor to each line in the active region
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
